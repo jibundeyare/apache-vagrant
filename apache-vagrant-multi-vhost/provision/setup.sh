2e49f55c9adb0930a81482cd531fc43241a12f59
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2018-02-21 11:40:01 +0100

    Amélioration du code

--- apache-vagrant-multi-vhost/provision/setup.sh
+++ apache-vagrant-multi-vhost/provision/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# update repositories
 apt-get update
 
 # install apache
@@ -42,20 +43,3 @@ systemctl restart apache2
 
 # install mysql
 apt-get install -y mysql-server mysql-client
-
-# # install phpmyadmin
-# # login: phpmyadmin
-# # password: 123
-# APP_PASS="123"
-# ROOT_PASS="123"
-# APP_DB_PASS="123"
-#
-# echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
-# echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | debconf-set-selections
-# echo "phpmyadmin phpmyadmin/mysql/admin-pass password $ROOT_PASS" | debconf-set-selections
-# echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_DB_PASS" | debconf-set-selections
-# echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
-#
-# apt-get install -y phpmyadmin
-#
-# echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
